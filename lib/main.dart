2eac01aa89e3e885ee77dcfa8f05fce6732d0772
hs <aids71214@gmail.com>
2019-12-07 16:51:26 +0800

    remove unused codes

--- lib/main.dart
+++ lib/main.dart
@@ -13,27 +13,20 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(
-          title: 'Flutter Demo Home Page', list: [0, 1, 2, 3, 4, 5, 6, 7]),
+          title: 'Expandable Text Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title, this.list}) : super(key: key);
+  MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
-  final List<int> list;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void _shuffle() {
-    setState(() {
-      widget.list.shuffle();
-      print("Shuffle: ${widget.list}");
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,25 +43,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class Tile extends StatefulWidget {
-  Tile(this.value);
-
-  final int value;
-
-  @override
-  _TileState createState() => _TileState();
-}
-
-class _TileState extends State<Tile> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      child: Text("${widget.value}", style: TextStyle(fontSize: 18)),
     );
   }
 }
